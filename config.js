683a50577fb1057d5a650bc3ef7558202d0f7205
dllsmm <itzaadil74@gmail.com>
2025-07-20 00:50:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Bot info kept here safely, and imported in script.js
-const BOT_TOKEN = " paste Bot token ";
-const CHAT_ID = " paste channel id ";
+const BOT_TOKEN = "8019092214:AAF3tM7YCkARMhfRkE9WcwOMTgcU7GSZd68";
+const CHAT_ID = "-1002722026924";
